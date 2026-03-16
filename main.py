8bdda9372a6a68b1ffdae9ddd20978e30520f488
Mohamed Abdelhafiz <oloom2006@gmail.com>
2026-03-17 01:01:20 +0200

    fix cloud run port issue

--- main.py
+++ main.py
@@ -589,3 +589,13 @@ th{{background:#1a73e8;color:white}}
 """
 
     return HTMLResponse(html)
+
+# =========================
+# RUN SERVER (Cloud Run fix)
+# =========================
+
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
